1a72cd55fbb030ca9d13a4634d36ed05638c928c
trolevan26299 <trolevan26299@gmail.com>
2023-07-06 14:09:55 +0700

    ext 1

---
+++ target/debug/deps/ex1_rust_levantro-9558fd6cc3c3929f.d
@@ -0,0 +1,5 @@
+/home/tro/Rust-Bootcamp-2023/ex1-rust-levantro/target/debug/deps/ex1_rust_levantro-9558fd6cc3c3929f.rmeta: src/main.rs
+
+/home/tro/Rust-Bootcamp-2023/ex1-rust-levantro/target/debug/deps/ex1_rust_levantro-9558fd6cc3c3929f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/ex1_rust_levantro-ad009199f3dba1f6.d
@@ -0,0 +1,5 @@
+/home/tro/Rust-Bootcamp-2023/ex1-rust-levantro/target/debug/deps/ex1_rust_levantro-ad009199f3dba1f6.rmeta: src/main.rs
+
+/home/tro/Rust-Bootcamp-2023/ex1-rust-levantro/target/debug/deps/ex1_rust_levantro-ad009199f3dba1f6.d: src/main.rs
+
+src/main.rs:
